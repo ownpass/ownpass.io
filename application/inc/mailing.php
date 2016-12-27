5009251d8da86911afe2ed955d019c81bc23ae6e
Walter Tamboer <walter@tamboer.nl>
2016-12-27 10:49:11 +0100

    Hooked up the mailing list subscription.

--- application/inc/mailing.php
+++ application/inc/mailing.php
@@ -1,5 +1,5 @@
 <section id="mailing">
-    <form action="" method="post">
+    <form action="//ownpass.us14.list-manage.com/subscribe/post?u=d7b5c2701e00fda26a898207d&amp;id=12829806c3" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
         <div class="wrap container-fluid">
             <div class="row">
                 <div class="col-xs-12">
@@ -10,10 +10,11 @@
             </div>
             <div class="row">
                 <div class="col-xs-12 col-sm-5 col-sm-offset-2">
-                    <input type="email" name="mailing-email" id="mailing-email" placeholder="email@address.com">
+                    <input type="email" name="EMAIL" id="mailing-email" placeholder="email@address.com">
+                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_d7b5c2701e00fda26a898207d_12829806c3" tabindex="-1" value=""></div>
                 </div>
                 <div class="col-xs-12 col-sm-3 text-right">
-                    <button>
+                    <button name="subscribe">
                         Sign up
                     </button>
                 </div>
